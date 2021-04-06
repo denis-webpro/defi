0f7d3878ffcaa489bbd2dae56a9e325b217068c0
denis-webpro <denis.webpro@gmail.com>
2021-04-06 20:14:24 +0200

    Exercice Node débutant - Cow Say!

---
+++ Wild Code School/defi/index.js
@@ -0,0 +1,11 @@
+const myInformation = require('./information.js');
+const chalk = require('cowsay');
+myInformation();
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "I'm a moooodule",
+	e : "oO",
+	T : "U "
+}));
---
+++ Wild Code School/defi/information.js
@@ -0,0 +1,8 @@
+let name = 'Denis';
+let campus = 'Wild Code School of Reims';
+
+const myInfos = () => {
+    console.log(`Hi my name is ${name}, I'm ${campus}`);
+};
+
+module.exports = myInfos;
